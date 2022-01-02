a46f04c2e498c84f21c094fe4468ac9766f79c96
J-Laur <jessica.lauria@csi.cuny.edu>
2022-01-01 21:45:12 -0500

    Counts how many booleans and strings are in an array

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,6 +1,18 @@
 function countOfAllBooleansAndStrings(arr) {
   // Insert code here;
+
+  let count = 0;
+
+  for(let i = 0; i < arr.length; i++)
+    if (typeof(arr[i]) == "boolean" || typeof(arr[i]) == "string"){
+      count++;
+    }
+
+  console.log(count);
+
 }
+
+countOfAllBooleansAndStrings([true, "Hello", false, "false", 7, Infinity]);
 
 // Do not edit this line;
 module.exports = countOfAllBooleansAndStrings;
