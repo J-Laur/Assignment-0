2040c29889c69cab581ca2a3fcd1536cc6ecf057
J-Laur <jessica.lauria@csi.cuny.edu>
2022-01-02 00:44:00 -0500

    Checks a string to see if it is a palindrome

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,6 +1,21 @@
 function isPalindrome(word) {
   // Insert code here;
-}
+
+  const len = word.length;  
+  
+    for (let i = 0; i < len / 2; i++) {  
+  
+        // validate the first and last characters are same  
+        if (word[i] !== word[len - 1 - i]) {  
+            console.log(false);  
+        }
+    }   
+    console.log(true); 
+}  
+
+isPalindrome("racecar");
+isPalindrome("book");
+
 
 // Do not edit this line;
 module.exports = isPalindrome;
