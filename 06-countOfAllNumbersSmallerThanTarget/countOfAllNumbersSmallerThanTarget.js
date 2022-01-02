c37561719454e13f97952f7cc7e1ba6e2d93403d
J-Laur <jessica.lauria@csi.cuny.edu>
2022-01-01 21:56:58 -0500

    Counts the number of integers in an array that are less than the target number

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,6 +1,18 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
   // Insert code here;
+  let count = 0;
+
+  for(let i = 0; i < nums.length; i++)
+    if (nums[i] < target){
+      count++;
+    }
+
+  console.log(count);
+
 }
+
+countOfAllNumbersSmallerThanTarget([3, 4, 7, 9, 0, 2], 8);
+
 
 // Do not edit this line;
 module.exports = countOfAllNumbersSmallerThanTarget;
