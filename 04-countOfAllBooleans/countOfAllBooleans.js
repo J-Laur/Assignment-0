61a14a519036ba168ed956b3e69db750a86f1a3d
J-Laur <jessica.lauria@csi.cuny.edu>
2022-01-01 21:28:46 -0500

    Counts the number of boolean values in an array

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,6 +1,19 @@
 function countOfAllBooleans(arr) {
   // Insert code here;
+
+  let count = 0;
+
+  for(let i = 0; i < arr.length; i++)
+    if (typeof(arr[i]) == "boolean"){
+      count++;
+    }
+
+  console.log(count);
+
 }
+
+countOfAllBooleans([true, "Hello", false]);
+
 
 // Do not edit this line;
 module.exports = countOfAllBooleans;
