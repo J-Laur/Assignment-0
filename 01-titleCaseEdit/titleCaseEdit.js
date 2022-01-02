922bae30803df4a4aa1498a816952253ea496352
J-Laur <jessica.lauria@csi.cuny.edu>
2022-01-01 20:11:59 -0500

    Second assignment completed

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,6 +1,17 @@
 function titleCaseEdit(title) {
   // Insert code here;
+
+  const words = title.split(" ");
+
+  for (let i = 0; i < words.length; i++) {
+      words[i] = words[i][0].toUpperCase() + words[i].substr(1);
 }
+
+return words.join(" ");
+
+}
+
+console.log(titleCaseEdit("goodight moon"));
 
 // Do not edit this line;
 module.exports = titleCaseEdit;
