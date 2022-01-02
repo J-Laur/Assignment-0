13f58662aefec12a8e17eb438ea38c694c091858
J-Laur <jessica.lauria@csi.cuny.edu>
2022-01-01 20:30:08 -0500

    Divides an integer in half and rounds it down

--- 03-cutInHalfAndFloor/cutInHalfAndFloor.js
+++ 03-cutInHalfAndFloor/cutInHalfAndFloor.js
@@ -1,6 +1,14 @@
 function cutInHalfAndFloor(num) {
   // Insert code here;
+
+  let divideBy = 2;
+  let result = num / divideBy;
+  let resultRoundedDown = Math.floor(result);
+
+  console.log(resultRoundedDown);
 }
+
+cutInHalfAndFloor(29);
 
 // Do not edit this line;
 module.exports = cutInHalfAndFloor;
