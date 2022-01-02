ec37562e7a5346d52fb704d0d109341dfc3db8c2
J-Laur <jessica.lauria@csi.cuny.edu>
2022-01-02 00:30:27 -0500

    Replaces various array elements with "fizz", "buzz", or "fizzbuzz"

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,6 +1,30 @@
 function fizzBuzz(start, end) {
-  // Insert code here;
+  // Insert code here;i
+  
+  let arr = new Array;
+
+  for(let i = start; i <= end; i++){
+    if (i % 3 == 0){
+      arr[i] = "Fizz";
+      console.log(arr[i]);
+    }
+    else if (i % 5 == 0){
+      arr[i] = "Buzz";
+      console.log(arr[i]);
+    }
+    else if (i % 3 == 0 && i % 5 == 0){
+      arr[i] = "FizzBuzz";
+      console.log(arr[i]);
+    }
+    else 
+      console.log(i);
+  
+  }
+
 }
+
+fizzBuzz(2, 20);
+
 
 // Do not edit this line;
 module.exports = fizzBuzz;
