3be0b6e118a7c6cbb8898da84cdea551b417ce06
J-Laur <jessica.lauria@csi.cuny.edu>
2022-01-01 20:20:01 -0500

    Completed

--- 02-subtractFive/subtractFive.js
+++ 02-subtractFive/subtractFive.js
@@ -1,6 +1,13 @@
 function subtractFive(num) {
   // Insert code here;
+
+  let subNum = 5;
+  let result = num - subNum;
+
+  console.log(result)
 }
+
+subtractFive(8);
 
 // Do not edit this line;
 module.exports = subtractFive;
