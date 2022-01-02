eb16a911a38e1b941b9bf1efec77ee95572f6c24
J-Laur <jessica.lauria@csi.cuny.edu>
2022-01-02 14:30:28 -0500

    Uses binary search to find a target number within an array

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -5,8 +5,29 @@ class MySolution {
 
   binarySearch(nums, target) {
     // Insert code here;
+
+    let arr = Array.from(nums); //creates an array
+    let first = 0;
+    let mid = Math.floor(arr.length/2);
+    let last = arr.length-1;
+
+    if(target == arr[mid]){
+      return true;
+    }
+    else if (target < arr[mid]){
+      arr = arr.splice(first,mid); 
+      return this.binarySearch(arr, target);
+    }
+    else if(target > arr[mid]){ 
+      arr= arr.splice(mid+1,last); 
+      return this.binarySearch(arr, target);
+    }
+    return false; 
   }
 }
+
+let solution = new MySolution();
+console.log(solution.binarySearch([1,2,3,4,5], 3))
 
 // Do not edit this line;
 let StudentSolution = MySolution;
