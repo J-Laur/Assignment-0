7a9e7f4a4b729ac807d4486471728f7c7ec33882
J-Laur <jessica.lauria@csi.cuny.edu>
2022-01-01 22:07:01 -0500

    Counts the number of integers in an array that are odd

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,6 +1,19 @@
 function sumOfAllOddNumbers(nums) {
   // Insert code here;
+
+  let count = 0;
+
+  for(let i = 0; i < nums.length; i++)
+    if (nums[i] % 2 == 1){
+      count++;
+    }
+
+  console.log(count);
+
 }
+
+sumOfAllOddNumbers([3, 4, 7, 9, 0, 2, 9, 10]);
+
 
 // Do not edit this line;
 module.exports = sumOfAllOddNumbers;
