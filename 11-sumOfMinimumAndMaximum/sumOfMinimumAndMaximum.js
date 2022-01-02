01d0dd36e9eb3aa2773b989ef4aa6390d306db2c
J-Laur <jessica.lauria@csi.cuny.edu>
2022-01-02 14:35:05 -0500

    Finds the minimum and maximum values of an array and adds them together

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,6 +1,23 @@
 function sumOfMinimumAndMaximum(nums) {
   // Insert code here;
+
+  let min = nums[0];
+  let max = nums[0];
+  let sum = 0;
+
+  for(let i = 0; i < nums.length; i++){
+    if(min > nums[i]){
+      min = nums[i];
+    }
+    if(max < nums[i]){
+      max = nums[i];
+    }
+  }
+  sum = min + max;
+  return sum;
 }
+
+console.log(sumOfMinimumAndMaximum([5, 6, 7, 4, 2, 10]));
 
 // Do not edit this line;
 module.exports = sumOfMinimumAndMaximum;
