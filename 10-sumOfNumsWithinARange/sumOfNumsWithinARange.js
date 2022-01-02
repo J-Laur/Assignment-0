36f8557fcb3132e330a246f64f54a4435a5244be
J-Laur <jessica.lauria@csi.cuny.edu>
2022-01-01 22:32:43 -0500

    Algo that counts the number of integers within a certain numerical range

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,6 +1,20 @@
 function sumOfNumsWithinARange(nums, start, end) {
   // Insert code here;
+
+  
+  let count = 0;
+
+  for(let i = 0; i < nums.length; i++)
+    if (nums[i] >= start && nums[i] <= end){
+      count++;
+    }
+
+  console.log(count);
+
 }
+
+sumOfNumsWithinARange([3, 4, 7, 9, 0, 2, 9, 10, 7, 4], 2, 9);
+
 
 // Do not edit this line;
 module.exports = sumOfNumsWithinARange;
