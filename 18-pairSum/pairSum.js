6076bce732db6286d934ec0469d514192bf2963a
J-Laur <jessica.lauria@csi.cuny.edu>
2022-01-02 14:23:10 -0500

    Checks if any of the numbers in the given array will add up to the target number, returns true if such a pair exists

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,6 +1,20 @@
 function pairSum(nums, target) {
   // Insert code here;
+  
+  let temp=false;
+  let arr = Array.from(nums);
+
+  arr.forEach(index1 => {arr.forEach(index2 =>{
+    if(index1 + index2 == target){
+      temp = true;
+    }
+  })
+})
+  
+return temp;
 }
+
+console.log(pairSum([3, 4, 6, 7, 8, 9], 17));
 
 // Do not edit this line;
 module.exports = pairSum;
