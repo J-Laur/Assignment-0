39812d615342a5a3a57dcf2ccf03a59fd031e21a
J-Laur <jessica.lauria@csi.cuny.edu>
2022-01-02 01:07:40 -0500

    Recursive function to add all numbers counting down from the value chosen by user

--- 15-countDownSum/countDownSum.js
+++ 15-countDownSum/countDownSum.js
@@ -1,8 +1,19 @@
 class MySolution {
+
   countDownSum(num) {
     // Insert code here;
-  }
+
+    console.log(num);
+
+    let nextNumber = num - 1;
+
+    if (nextNumber > 0) {
+        countDownSum(nextNumber);
+    }
 }
+
+let studentSolution = new MySolution();
+studentSolution.countDownSum(5)
 
 // Do not edit this line;
 let StudentSolution = MySolution;
