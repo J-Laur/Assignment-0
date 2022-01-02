820c58cc6e267fd2430c30f173b50f741faf1538
J-Laur <jessica.lauria@csi.cuny.edu>
2022-01-01 19:40:19 -0500

    First assignment in Assignment 0 complete!

--- 00-greetFriend/greetFriend.js
+++ 00-greetFriend/greetFriend.js
@@ -1,6 +1,11 @@
 function greetFriend(name) {
   // Insert code here;
+
+  console.log('Hello, ' + name + '!');
+
 }
+
+greetFriend("Jess");
 
 // Do not edit this line;
 module.exports = greetFriend;
