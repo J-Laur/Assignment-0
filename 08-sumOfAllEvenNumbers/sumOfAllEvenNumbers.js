eefd0cf0d96f6fbf5dbcbbf0a35b600e171bab62
J-Laur <jessica.lauria@csi.cuny.edu>
2022-01-01 22:04:38 -0500

    Counts the number of integers in an array that are even

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,6 +1,19 @@
 function sumOfAllEvenNumbers(nums) {
   // Insert code here;
+
+  let count = 0;
+
+  for(let i = 0; i < nums.length; i++)
+    if (nums[i] % 2 == 0){
+      count++;
+    }
+
+  console.log(count);
+
 }
+
+sumOfAllEvenNumbers([3, 4, 7, 9, 0, 2]);
+
 
 // Do not edit this line;
 module.exports = sumOfAllEvenNumbers;
