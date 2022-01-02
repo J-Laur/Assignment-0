dc127fc1a97491415f4bbff5a487baf1289db069
J-Laur <jessica.lauria@csi.cuny.edu>
2022-01-02 01:14:57 -0500

    Finds the product of any given number of integers

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,6 +1,17 @@
 function productOfAnyAmountOfNumbers(...args) {
   // Insert code here;
+
+let product = 1;
+
+for (let i = 0; i < args.length; i++){
+
+  product *= args[i];
 }
+
+  console.log(product);
+}
+
+productOfAnyAmountOfNumbers(4, 5, 6, 7, 8)
 
 // Do not edit this line;
 module.exports = productOfAnyAmountOfNumbers;
